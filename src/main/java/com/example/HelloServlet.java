32cea4517915094a4552ff0e3329fb2f3cb42aea
sumukh624 <sumukh624@gmail.com>
2026-04-09 10:04:28 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,19 +1,27 @@
 package com.example;
 
-import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-                         throws ServletException, IOException {
+                        HttpServletResponse response)
+                        throws ServletException, IOException {
 
         response.setContentType("text/html");
+
         PrintWriter out = response.getWriter();
 
+        out.println("<html><body>");
         out.println("<h2>Deployment Successful!</h2>");
         out.println("<h3>GitHub → Jenkins → Docker → Tomcat</h3>");
+        out.println("</body></html>");
     }
 }
